4d24b4f88508673582fbb3a3c01a34e753f7a356
EricaReis <ericareis67@outlook.com>
2025-03-29 19:48:39 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Course/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Casting {
+    class Program
+    {
+        static void Main(string[] args) {
+            int a = 5;
+            int b = 2;
+
+            // Casting:
+            double result = (double) a / b;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Functions {
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type 3 numbers;");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double result = Bigger(n1, n2, n3);
+
+            Console.WriteLine(result);
+        }
+
+        static int Bigger(int a, int b, int c)
+        {
+            int m;
+
+            if (a > b && a > c)
+            {
+                m = a;
+            }
+            else if (b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+
+            return m;
+        }
+    }
+}
