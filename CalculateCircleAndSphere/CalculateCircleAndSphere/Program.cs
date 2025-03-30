8d384838438c18c17abdefa558fe5c9d37b463e6
EricaReis <ericareis67@outlook.com>
2025-03-30 17:40:07 -0300

    Static members

---
+++ CalculateCircleAndSphere/CalculateCircleAndSphere/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculateCircleAndSphereData {
+    class Program {
+        static double Pi = 3.14;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the radius: ");
+            double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circumference(radius);
+            double volumn = Volumn(radius);
+
+            Console.WriteLine("Circumference: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volumn: " + volumn.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Pi value: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        static double Circumference(double radius)
+        {
+            return 2.0 * Pi * radius;
+        }
+
+        static double Volumn(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
