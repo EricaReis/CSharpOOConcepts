4c7159b21cf5f4a978763a0aa72e522a98eecd6b
EricaReis <ericareis67@outlook.com>
2025-04-01 21:52:35 -0300

    Constructor and overload

---
+++ ConstructorClass/ConstructorClass/Product.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace ProductStock {
+    internal class Product {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        // Constructor:
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        // Constructor Overload
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public Product()
+        {
+        }
+
+        public double TotalValueInStock()
+        {
+            return Price * Quantity;
+        }
+
+        public void AddProducts(int quantity)
+        {
+            Quantity += quantity;
+        }
+
+        public void RemoveProducts(int quantity)
+        {
+            Quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", $" + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantity + " units, Total: $"
+                + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ConstructorClass/ConstructorClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProductStock {
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter product data: ");
+            Console.WriteLine("Name: ");
+            string  name = Console.ReadLine();
+
+            Console.WriteLine("Price: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantity: ");
+            int quantity = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Calling the Constructor
+            Product p = new Product(name, price, quantity);
+
+            // Overload
+            Product p2 = new Product(name, price);
+            Product p3 = new Product();
+
+            // Object syntax
+            Product p4 = new Product { Name = "TV", Price = 20, Quantity = 2 };
+
+            Console.WriteLine("Product info: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the quantity of products to add: ");
+            int qty = int.Parse(Console.ReadLine());
+
+            p.AddProducts(qty);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated data: " + p);
+
+
+            Console.WriteLine("Enter the quantity of products to remove: ");
+            qty = int.Parse(Console.ReadLine());
+            p.RemoveProducts(qty);
+            Console.WriteLine();
+            Console.WriteLine("Updated data: " + p);
+        }
+    }
+}
