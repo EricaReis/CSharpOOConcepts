8cfdfa887300ec5aedfb0c0fb1d06d956f90e6b7
EricaReis <ericareis67@outlook.com>
2025-04-23 19:56:09 -0300

    Ref modifier

---
+++ RefModifier/RefModifier/Calculator.cs
@@ -0,0 +1,11 @@
+﻿using RefModifier;
+
+namespace RefModifier {
+    class Calculator {
+        // Ref Modifier:
+        public static void Triple(ref int x)
+        {
+            x = x * 3;
+        }
+    }
+}
---
+++ RefModifier/RefModifier/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using RefModifier;
+
+class Program {
+    static void Main(string[] args)
+    {
+        int a = 10;
+        Calculator.Triple(ref a);
+        Console.WriteLine(a);
+    }
+}
