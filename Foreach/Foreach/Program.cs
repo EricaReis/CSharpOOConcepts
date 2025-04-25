c6af10ae46dc634b151645e68e655cba7abfa31f
EricaReis <ericareis67@outlook.com>
2025-04-24 21:44:37 -0300

    Lists

---
+++ Foreach/Foreach/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Foreach {
+    class Program {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Bob", "Alex" };
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
