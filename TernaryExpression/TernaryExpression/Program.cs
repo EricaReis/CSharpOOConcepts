4cff7d8bf1f71e2ef24be89f5130960195fb13c3
EricaReis <ericareis67@outlook.com>
2025-06-03 21:42:34 -0300

    TernaryExpressions and UsefulStringFunctions

---
+++ TernaryExpression/TernaryExpression/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization ;
+
+namespace TernaryExpression {
+    class Program {
+        static void Main(string[] args)
+        {
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double discount = (price < 20.0) ? price * 0.1 : price * 0.05;
+
+            Console.WriteLine(discount);
+        }
+    }
+}
