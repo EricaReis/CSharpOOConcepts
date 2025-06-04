68af8c576b7c5a58e64f940cdfe9ca062bb31aa3
EricaReis <ericareis67@outlook.com>
2025-06-03 21:21:48 -0300

    SwitchCase

---
+++ TypeInference/TypeInference/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypeInference {
+    class Program {
+        static void Main(string[] args)
+        {
+            // The compiler automatically deduces the type of the variable through type inference.
+            var x = 10;
+            var y = 20.0;
+        }
+    } 
+}
